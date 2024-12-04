0f4bb47bd6b203dc96207791554b61a8d70d5ce2
SexySlave <137678642+SexySlave@users.noreply.github.com>
2024-12-04 12:02:18 +0300

    Initial

---
+++ myproject/myapp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
